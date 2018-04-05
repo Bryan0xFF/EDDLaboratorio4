965c4cbbe1e7c32c69049857dd573f010656661e
Bryan0xFF <bjmejia@correo.url.edu.gt>
2018-04-05 08:49:03 -0600

    arreglar tipo dato Pais

--- Laboratorio4_1252016_1053016/Laboratorio4_1252016_1053016/Models/Pais.cs
+++ Laboratorio4_1252016_1053016/Laboratorio4_1252016_1053016/Models/Pais.cs
@@ -7,9 +7,9 @@ namespace Laboratorio4_1252016_1053016.Models
 {
     public class Pais
     {
-        public string Faltantes { get; set; }
-        public string Coleccionadas { get; set; }
-        public string Cambios { get; set; }
+        public int Faltantes { get; set; }
+        public int Coleccionadas { get; set; }
+        public int Cambios { get; set; }
 
     }
 }
