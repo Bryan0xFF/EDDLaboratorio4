ebf87311f4febc181a535c7b0454c7c6f9cab2e3
Bryan0xFF <bjmejia@correo.url.edu.gt>
2018-04-05 07:59:31 -0600

    Inicio laboratorio

---
+++ Laboratorio4_1252016_1053016/Laboratorio4_1252016_1053016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio4_1252016_1053016.MvcApplication" Language="C#" %>
---
+++ Laboratorio4_1252016_1053016/Laboratorio4_1252016_1053016/Models/Pais.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio4_1252016_1053016.Models
+{
+    public class Pais
+    {
+        public string Faltantes { get; set; }
+        public string Coleccionadas { get; set; }
+        public string Cambios { get; set; }
+
+    }
+}
